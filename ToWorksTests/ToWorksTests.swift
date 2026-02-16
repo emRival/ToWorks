ed205d59aa68e64000e781a3ad0f2f8b37bb7828
Rival-Dev <em.rival282@gmail.com>
2026-02-16 08:23:37 +0700

    Initial Commit

---
+++ ToWorksTests/ToWorksTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToWorksTests.swift
+//  ToWorksTests
+//
+//  Created by RIVAL on 16/02/26.
+//
+
+import Testing
+@testable import ToWorks
+
+struct ToWorksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
